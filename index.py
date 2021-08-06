ce35a88ebc2daf0f222a757dd16c1e4cbe564e2d
Yegor Kazantsev <wwwadmin@bk.ru>
2021-08-06 19:00:07 +0300

    export models

--- index.py
+++ index.py
@@ -3,15 +3,24 @@ import os
 
 import bottle
 from bottle import hook
+from bottle import HTTPError
+from bottle import request
 from bottle import response
 from bottle import route
 from bottle import run
 from bottle import template
 from peewee import fn
+from playhouse.shortcuts import model_to_dict
 
 from db import db
 from models import Vacancy
 
+ACCESS_DENIED_STR: str = "Access Denied"
+CORS_ALL_WILDCARD: str = "*"
+CORS_ALLOWED_HTTP_METHODS: str = "GET, POST, PUT, OPTIONS"
+CORS_ALLOWED_HTTP_HEADERS: str = (
+    "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
+)
 """
 Enable Sentry if needed
 """
@@ -47,16 +56,21 @@ def _close_db():
 
 @hook("after_request")
 def _enable_cors():
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, OPTIONS"
-    response.headers[
-        "Access-Control-Allow-Headers"
-    ] = "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
+    response.headers["Access-Control-Allow-Origin"] = CORS_ALL_WILDCARD
+    response.headers["Access-Control-Allow-Methods"] = CORS_ALLOWED_HTTP_METHODS
+    response.headers["Access-Control-Allow-Headers"] = CORS_ALLOWED_HTTP_HEADERS
 
 
 """
 Routes
 """
+
+
+@route("/export-vacancy/")
+def export_vacancy():
+    if request.query.get("access_token") != os.environ("ACCESS_MAGIC_KEY", None):
+        raise HTTPError(status=403, body=ACCESS_DENIED_STR)
+    return json.dumps(model_to_dict(Vacancy.select()))
 
 
 @route("/count-by-date-chart/")
